2729457d832e3d2fef6a1254b5828e1825f78e57
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2022-09-08 23:24:38 -0300

    fixed the location issue

--- src/app/api-connection.service.ts
+++ src/app/api-connection.service.ts
@@ -10,40 +10,20 @@ export class ApiConnectionService {
   private apiKey: string = '749b37114amshf7430399b5fb346p1b59e2jsn451653c0fe81';
   private apiHost: string = 'weatherbit-v1-mashape.p.rapidapi.com';
 
-  lat: number = this.getLat();
-  lon: number = this.getLon();
-
-  options: object = {
-    headers: {
-      'X-RapidAPI-Host': this.apiHost,
-      'X-RapidAPI-Key': this.apiKey
-    },
-    params: {
-      'lang': 'en',
-      'lat': this.lat,
-      'lon': this.lon
-    },
-    responseType: 'json'
-  }
-
-  getData() {
-    return this.http.get<IForecast>(this.apiUrl, this.options)
-  }
-
-  getLat() {
-    let tempLat: number = 0;
-    navigator.geolocation.getCurrentPosition(
-      (ans) => {tempLat = ans.coords.latitude; console.log("NEW LATITUDE: " + tempLat);}
-    )
-    return tempLat;
-  }
-
-  getLon() {
-    let tempLon: number = 0;
-    navigator.geolocation.getCurrentPosition(
-      (ans) => {tempLon = ans.coords.longitude; console.log("NEW LONGITUDE: " + tempLon)}
-    )
-    return tempLon;
+  getData(lat: number, lon: number) {
+    let options: object = {
+      headers: {
+        'X-RapidAPI-Host': this.apiHost,
+        'X-RapidAPI-Key': this.apiKey
+      },
+      params: {
+        'lang': 'en',
+        'lat': lat,
+        'lon': lon
+      },
+      responseType: 'json'
+    }
+    return this.http.get<IForecast>(this.apiUrl, options)
   }
 
   constructor(private http: HttpClient) { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,10 @@ import { IForecast } from './forecastInterface';
 export class AppComponent implements OnInit {
   forecast: IForecast;
 
+  // Location Info
+  lat: number;
+  lon: number;
+
   // Header Info
   city: string;
   date: Date = new Date();
@@ -24,20 +28,27 @@ export class AppComponent implements OnInit {
   humidity: number;
 
   ngOnInit(): void {
-    this.apiService.getData()
-      .subscribe(res => {
-        this.forecast = res;
-        // Header
-        this.city = this.forecast.city_name;
-        // Forecast
-        this.desc = this.forecast.data[0].weather.description;
 
-        this.icon = `https://www.weatherbit.io/static/img/icons/${this.forecast.data[0].weather.icon}.png`;
+    navigator.geolocation.getCurrentPosition( res => {
+      this.lat = res.coords.latitude;
+      this.lon = res.coords.longitude;
+      
+      this.apiService.getData(this.lat, this.lon)
+        .subscribe(res => {
+          this.forecast = res;
+          // Header
+          this.city = this.forecast.city_name;
+          // Forecast
+          this.desc = this.forecast.data[0].weather.description;
+  
+          this.icon = `https://www.weatherbit.io/static/img/icons/${this.forecast.data[0].weather.icon}.png`;
+  
+          this.temp = this.forecast.data[0].temp;
+          this.wind = this.forecast.data[0].wind_spd;
+          this.humidity = this.forecast.data[0].rh;
+        })
+    })
 
-        this.temp = this.forecast.data[0].temp;
-        this.wind = this.forecast.data[0].wind_spd;
-        this.humidity = this.forecast.data[0].rh;
-      })
   }
   
   constructor(private apiService: ApiConnectionService) { }
